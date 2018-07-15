2f1792c5f3860335464c9cdc146b609d4599de5c
Chetna Alreja <chetnaalreja91@gmail.com>
2018-07-15 23:00:30 +0530

    Add files via upload

---
+++ PyFundamentals_1.py
@@ -0,0 +1,44 @@
+#This is a comment.
+print("Hello, World!")
+"""This is a 
+multiline docstring.docstrings are also comments"""
+print("Hello, World!")
+#indentation is important in python
+
+
+#Declaring Variables and passing values
+x = 5
+y = "John"
+print(x)
+print(y)
+
+age=55
+john=reema=mia=55
+john,lia,mia =55,35,25
+
+#combiningstrings
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+x = "awesome"
+
+#types
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+'''To Verify the Type'''
+print(type(x))
+print(type(y))
+print(type(z))
+
+'''output:
+<class 'int'>
+<class 'float'>
+<class 'complex'>'''
+print("Python is " + x)
+
+#strings indixes
+fruit='banana'
+x=len(fruit)
+print(x)  3output6
